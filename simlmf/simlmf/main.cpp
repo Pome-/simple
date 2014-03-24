196e3c546fff2d50b0d4614976ca3de8190a8955
Pome- <prepome@gmailcom>
2014-03-24 10:47:38 +0900

    INIT
    CREATE

---
+++ simlmf/simlmf/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  simlmf
+//
+//  Created by 강은철 on 2014. 3. 19..
+//  Copyright (c) 2014년 강은철. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
